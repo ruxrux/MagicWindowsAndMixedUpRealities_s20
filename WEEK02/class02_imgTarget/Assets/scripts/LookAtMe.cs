fc1ae268d1feb94cc13ddb7f7839546fa2410f16
rux <rux@ruxs-MacBook-Pro.local>
2020-02-07 13:26:27 -0500

    added class 02 example
    image targets

---
+++ WEEK02/class02_imgTarget/Assets/scripts/LookAtMe.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMe : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(target);
+    }
+}
