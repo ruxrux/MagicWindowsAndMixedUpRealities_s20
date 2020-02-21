16a19f076e1c7718f8aa54a69161f97c12435add
ruxrux <rux.pgp@gmail.com>
2020-02-21 11:33:54 -0500

    added class 04 example
    includes model target + get data

---
+++ WEEK04/Class04_ModelTarget+GetDta/Assets/Scripts/getRequest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using SimpleJSON;
+
+public class getRequest : MonoBehaviour
+{
+    string location;
+    string weather;
+
+    void Start()
+    {
+        StartCoroutine(GetText());
+    }
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("api.openweathermap.org/data/2.5/weather?q=Brooklyn&appid=702367da6775090551215694c7a7fea9");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            //Debug.Log(www.downloadHandler.text);
+
+            // read as JSON
+            var result = JSON.Parse(www.downloadHandler.text);
+
+            // parse JSON
+
+            weather = result["weather"][0]["description"].Value;
+            location = result["name"].Value;
+            Debug.Log(location + " has " + weather);
+
+
+        }
+    }
+}
